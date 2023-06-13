1988cb7b0c56567cac1cb2e4a159476048fe2dd3
unknown <miagaitan97@gmail.com>
2023-06-13 00:12:38 -0300

    Pantallas2

--- public/assets/scenes/Controles.js
+++ public/assets/scenes/Controles.js
@@ -3,4 +3,16 @@ export default class Controles extends Phaser.Scene {
 
       super("Controles");
     }
+    init () {
+
+    }
+    preload () {
+      this.load.image("PControles", "./public/assets/images/PControles.png")
+      this.load.image("Menu", "./public/assets/images/Menu.png")
+    }
+    create () {
+      this.add.image(400, 300, "PControles").setScale(1);
+      this.add.image(400, 550, "Menu").setScale(1).setInteractive()
+      .on("pointerdown", () => this.scene.start("Menu"));;
+}
 }
