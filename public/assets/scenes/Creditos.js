5916f3f9b499f7513ef7b10d6b74cbbbd3ae66b8
unknown <miagaitan97@gmail.com>
2023-06-12 23:29:41 -0300

    Pantallas

--- public/assets/scenes/Creditos.js
+++ public/assets/scenes/Creditos.js
@@ -3,4 +3,17 @@ export default class Creditos extends Phaser.Scene {
 
       super("Creditos");
     }
+    init () {
+
+    }
+    preload () {
+      this.load.image("PCreditos", "./public/assets/images/PCreditos.png")
+      this.load.image("Menu", "./public/assets/images/Menu.png")
+    }
+    create () {
+      this.add.image(400, 300, "PCreditos").setScale(1);
+      this.add.image(400, 550, "Menu").setScale(1).setInteractive()
+      .on("pointerdown", () => this.scene.start("Menu"));;
 }
+}
+
--- public/assets/scenes/Menu.js
+++ public/assets/scenes/Menu.js
@@ -8,11 +8,20 @@ init () {
 
 }
 preload () {
-    this.load.image("Menu", "./public/assets/images/Inicio.png")
+    this.load.image("Inicio", "./public/assets/images/Inicio.png")
+    this.load.image("Jugar", "./public/assets/images/Jugar.png")
+    this.load.image("Controles", "./public/assets/images/Controles.png")
+    this.load.image("Creditos", "./public/assets/images/Creditos.png")
 } 
 
 create () {
-this.add.image(400, 300, "Menu").setScale(1);
+this.add.image(400, 300, "Inicio").setScale(1);
+this.add.image(240, 380, "Jugar").setScale(1).setInteractive()
+.on("pointerdown", () => this.scene.start("Niveles"));
+this.add.image(245, 440, "Controles").setScale(1).setInteractive()
+.on("pointerdown", () => this.scene.start("Controles"));
+this.add.image(240, 500, "Creditos").setScale(1).setInteractive()
+.on("pointerdown", () => this.scene.start("Creditos"));;
 
 }
 }
